ea52a3a0a573d3b093babe3a45e553643e39b6f2
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-21 17:35:37 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -6,6 +6,7 @@ class Comment < ActiveRecord::Base
 
   def user_attributes=(user_attributes)
     user_attributes.values.each do |user_attribute|
+      binding.pry
       user = User.find_or_create_by(user_attribute)
       self.user = user
     end
