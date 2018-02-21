530696720bc01a97739e967c91b66a35755552d3
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-02-21 17:50:47 +0000

    automatically backed up by learn

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,7 +5,6 @@ class Comment < ActiveRecord::Base
   accepts_nested_attributes_for :user, reject_if: proc { |attributes| attributes['username'].blank? }
 
   def user_attributes=(user_attributes)
-    raise user_attributes.inspect
     user_attributes.each do |user_attribute|
       user = User.find_or_create_by(username: user_attribute)
       self.user = user
